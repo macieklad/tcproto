1d20c33db3b9e938fd5f111cdba3182a6180ce26
macieklad <macieklad@gmail.com>
2020-05-11 22:45:54 +0200

    feat: ok response with content

--- pkg/proto/msg.go
+++ pkg/proto/msg.go
@@ -2,11 +2,14 @@ package proto
 
 import "fmt"
 
-
 func Error(msg string) []byte {
 	return []byte(fmt.Sprintf("ERR %s \n", msg))
 }
 
 func Ok() []byte {
 	return []byte("OK\n")
+}
+
+func OkMsg(msg string) []byte {
+	return []byte(fmt.Sprintf("OK %s \n", msg))
 }
